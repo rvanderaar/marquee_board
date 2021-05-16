e100dc6de76464ffd85dd518b69a2cb4e5725d41
Ramón van der Aar <r.vander.aar@hotmail.com>
2021-05-16 19:22:04 +0200

    Hardcoded rootfs path so it works on root account as well.

--- build_script.sh
+++ build_script.sh
@@ -10,7 +10,7 @@ cd $DIR/build
 # To get the rootfs which is required here, use:
 # rsync -rl --delete-after --safe-links pi@192.168.1.PI:/{lib,usr} $HOME/rpi/rootfs
 
-export RASPBIAN_ROOTFS=$HOME/raspi/rootfs
+export RASPBIAN_ROOTFS=/home/develop/raspi/rootfs
 export PATH=/opt/cross-pi-gcc/bin:/opt/cross-pi-gcc/libexec/gcc/arm-linux-gnueabihf/8.3.0:$PATH
 export RASPBERRY_VERSION=1
 
